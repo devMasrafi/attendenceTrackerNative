716231e8118fb99fb42374d3a57539645ac35aad
Md. Masrafi Mondol <masrafisw@gmail.com>
2025-01-23 23:32:57 +0600

    setup

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+import "./global.css"
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function RootLayout() {
+  return (
+    <Stack/>
+  );
+}
